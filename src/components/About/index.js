569366988dc3d73fcdf8900f3fe12aed3a72bb31
Stephanie <stephanieanderson5133@gmail.com>
2022-05-05 15:50:14 -0500

    initial setup

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <div className="my-2">
+        <p>
+         I am Stephanie
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
