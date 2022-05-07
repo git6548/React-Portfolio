cbc116fc3c8607f9eccdbb3fcc10633ee41930fb
Stephanie <stephanieanderson5133@gmail.com>
2022-05-07 15:55:27 -0500

    added the different pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-
-  return (
-    <div>
-      <Nav />
-      <main>
-          <About />        
-      </main>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>Stephanie Anderson</h2>
+      <img src="..\..\assets\images\avatar-image.jpg" alt="avatar image" />
+      <p>
+        Hello, my name is Stephanie Anderson.
+      </p>
+    </div>
+  );
+}
