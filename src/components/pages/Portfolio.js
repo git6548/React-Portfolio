68fdf40b5ee6dca31eb58098a9449c7e7e7b1f05
Stephanie <stephanieanderson5133@gmail.com>
2022-05-14 14:23:50 -0500

    some changes to style

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import screenshot_forecast from '../../assets/images/Screenshot-ForecastFilms.jpg';
-import screenshot_gameporium from '../../assets/images/Screenshot-Gameporium.jpg';
-import mountain_image from '../../assets/images/mountain-image.jpg';
+// import screenshot_forecast from '../../assets/images/Screenshot-ForecastFilms.jpg';
+// import screenshot_gameporium from '../../assets/images/Screenshot-Gameporium.jpg';
+// import mountain_image from '../../assets/images/mountain-image.jpg';
 
 export default function Portfolio() {
   return (
@@ -9,26 +9,34 @@ export default function Portfolio() {
         <h1>Stephanie Anderson</h1>
         <h2>Portfolio</h2>
         <div class="mainContainer">
-            <div className = "smallContainer1 forcastFilms">
+            <div className = "smallContainer forcastFilms">
               <div>
                 <a href="https://git6548.github.io/ForecastFilms/">
                 {/* <img src={screenshot_forecast} alt="Forecast Films" /> */}
-                Delpoyed Site
+                Forecast Films Delpoyed Site
                 </a>
                 <a href="https://github.com/git6548/ForecastFilms">Forecast Films Repo</a>
                 </div>
             </div>
-            <div class="smallContainer2">
+            <div class="smallContainer gameporium">
+            <div>
                     <a href="https://morning-lowlands-91275.herokuapp.com/">
-                    <img src="../assets/images/Screenshot-Gameporium.jpg" alt="Gameporium" />
+                    {/* <img src="../assets/images/Screenshot-Gameporium.jpg" alt="Gameporium" /> */}
+                   GamePorium Deployed Site
                     </a>
                     <a href=" https://github.com/git6548/Gameporium">Gameporium Repo</a>
                 </div>
-                <div class = "smallContainer3">
+                </div>
+                <div class = "smallContainer project3">
+                  <div>
                     <a href="#">
-                    <img src="./assets/images/mountain-image.jpg" alt="Project 3" />
-                    <p>Project 3</p>
+                      Project 3 Deployed Site
+                      </a>
+                    {/* <img src="./assets/images/mountain-image.jpg" alt="Project 3" /> */}
+                    {/* <p>Project 3</p> */}
+                    <a href= "#"> Project 3 Repo
                     </a>
+                    </div>
                 </div>
             </div>
 
